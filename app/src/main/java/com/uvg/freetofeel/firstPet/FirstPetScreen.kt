0ede82682bdf63f453a5579c36aac0c1ee195666
Renato Rojas <roj23813@uvg.edu.gt>
2024-09-20 21:40:01 -0600

    Se aregegó la pantalla de FirstPet

--- app/src/main/java/com/uvg/freetofeel/firstPet/FirstPetScreen.kt
+++ app/src/main/java/com/uvg/freetofeel/firstPet/FirstPetScreen.kt
@@ -1,2 +1,96 @@
 package com.uvg.freetofeel.firstPet
 
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.uvg.freetofeel.LanguageViewModel
+import com.uvg.freetofeel.R
+import com.uvg.freetofeel.loginBase.LoginBaseScreen
+import com.uvg.freetofeel.newAccount.NewAccountScreen
+@Composable
+fun FirstPetScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.inversePrimary), // Cambiar cuando se tengan los colores reales
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.lazy_back), // Se planeó que el perezoso sea el primero en salir
+            contentDescription = "Hábitat del perezoso",
+            alignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillHeight
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        AlertDialog(
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer.copy(alpha = 0.4f),
+            onDismissRequest = { /* Lógica al cerrar */ }, //posiblemente se quede vacío, porque solo hay aceptar
+            title = { Text(text = stringResource(id = R.string.first_pet_title)) },
+            text = { Text(text = stringResource(id = R.string.first_pet_body)) },
+            confirmButton = {
+                Button(onClick = { /* Lógica de aceptar */ }) {
+                    Text(text = stringResource(id = R.string.first_pet_confirm))
+                }
+            }
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFirstPetScreenLight() {
+    MaterialTheme(colorScheme = lightColorScheme()) {
+        val languageViewModel = LanguageViewModel()
+        FirstPetScreen()
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewFirstPetScreenDark() {
+    MaterialTheme(
+        colorScheme = darkColorScheme() // DarkMode
+    ) {
+        FirstPetScreen()
+    }
+}
